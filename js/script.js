d690467dc54bc62c5dd3435b55d880ba1322fbaa
haydar <vsa.haydar@gmail.com>
2020-08-12 02:22:55 +0200

    login func is klaar

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+//Account Aanmaken 
+document.getElementById("signUp").addEventListener('click',function (){
+let sEmail=document.getElementById('modalLRInput12').value
+let sWachtwoord=document.getElementById('modalLRInput13').value;
+let sHerWachtwoord=document.getElementById('modalLRInput14').value;
+let url='http://localhost:3000/gebruikers'
+let newGebruiker={
+  login: sEmail,
+  wachtwoord:sWachtwoord,
+}
+if((newGebruiker.login.length!==0 && newGebruiker.wachtwoord.length!==0)&& newGebruiker.wachtwoord ===sHerWachtwoord){
+   const xhr = new XMLHttpRequest();
+   xhr.open('POST', url, true);
+   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+   xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4 && xhr.status === 201){
+       const serverResponse = JSON.parse(xhr.response);
+       console.log(serverResponse)
+     }
+   };
+   xhr.send(JSON.stringify(newGebruiker));
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
