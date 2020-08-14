c6a0970b13fef06eef84977d8289bd19f7081d71
haydar <vsa.haydar@gmail.com>
2020-08-14 23:56:09 +0200

    Cuma aksam

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
+let speltjes=[
+  {
+    "name":"Civilization6",
+    "prijs":59.99,
+    "url":"https://civilization.com/",
+  },
+  {
+    "name":"Hell Let Loose",
+    "prijs":29.99,
+    "url":"https://store.steampowered.com/app/686810/Hell_Let_Loose/",
+  },
+  {
+    "name":"Escape From Tarkov",
+    "prijs":29.74,
+    "url":"https://www.escapefromtarkov.com/",
+  },
+]
+
+// Put the object into storage
+localStorage.setItem('wnkl', JSON.stringify(speltjes));
+// Retrieve the object from storage
+var retrievedObject = JSON.parse(localStorage.getItem('wnkl'));
+
+console.log('retrievedObject: ', retrievedObject);
+
 
 //Account Aanmaken 
 document.getElementById("signUp").addEventListener('click',function (){
-  // debugger
 let sEmail=document.getElementById('modalLRInput12');
 let sWachtwoord=document.getElementById('modalLRInput13');
 let sHerWachtwoord=document.getElementById('modalLRInput14');
@@ -14,7 +38,7 @@ if((newGebruiker.login.length!==0 && newGebruiker.wachtwoord.length!==0)&& newGe
    const xhr = new XMLHttpRequest();
    xhr.open('POST', url, true);
    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-   xhr.onreadystatechange = ()=> {
+   xhr.onreadystatechange = () => {
      if (xhr.readyState === 4 && xhr.status === 201){
        let serverResponse = JSON.parse(xhr.response);
        console.log(serverResponse)
@@ -22,7 +46,9 @@ if((newGebruiker.login.length!==0 && newGebruiker.wachtwoord.length!==0)&& newGe
    };
    xhr.send(JSON.stringify(newGebruiker));
   }
-  sEmail.value=''
+  sEmail.value='';
+  sWachtwoord='';
+  sHerWachtwoord='';
 });
 
 
@@ -41,7 +67,7 @@ document.getElementById('logIn').addEventListener('click',function(){
   })){
     sEmail.value='';
     sWachtwoord.value='';
-    // window.location = "index2.html";
+    window.location = "Dames.html";
   }else{
     sEmail.value='';
     sWachtwoord.value='';
